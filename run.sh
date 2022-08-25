42c9b6b893586e0a471bbce3736088d52e46bffe
Dayong Wang <dayongwang@fb.com>
2022-08-25 11:28:42 -0400

    update zshrc

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 rm .vimrc .screenrc .vim .bash_aliases
 
 ln -s ~/config/vimrc .vimrc
+ln -s ~/config/zshrc .zshrc
 ln -s ~/config/screenrc .screenrc
 ln -s ~/config/bash_aliases .bash_aliases
 ln -s ~/config/tmux.conf .tmux.conf
