c8e53cf980d2b5e53efedf5500ae434d4021be2a
Dayong Wang <dayongwang@fb.com>
2022-08-25 11:31:01 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+# Install zsh package
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+#
 rm .vimrc .screenrc .vim .bash_aliases
 
 ln -s ~/config/vimrc .vimrc
